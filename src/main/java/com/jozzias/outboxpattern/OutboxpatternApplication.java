42e693305ee3abd1d4a19d65505f36dd8f555688
Aelcio Jozias <aelciojoziasp@gmail.com>
2024-03-22 19:43:10 -0300

    Initial commit

---
+++ src/main/java/com/jozzias/outboxpattern/OutboxpatternApplication.java
@@ -0,0 +1,13 @@
+package com.jozzias.outboxpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutboxpatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutboxpatternApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jozzias/outboxpattern/OutboxpatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jozzias.outboxpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutboxpatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
